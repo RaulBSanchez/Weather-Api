bef50ca480c565491d7c6495c2ae708c90a8f55a
Raul Bazan Sanchez <47358801+RaulBSanchez@users.noreply.github.com>
2019-11-27 20:24:56 -0500

    updates

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', ()=> {
 	let temperatureDescription = document.querySelector(".temperature-description");
 	let temperatureDegree = document.querySelector(".temperature-degree");
 	let locationTimezone = document.querySelector(".location-timezone");
+	let temperatureSection = document.querySelector(".temperature");
+	const temperatureSpan = document.querySelector(".temperature span");
 
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
@@ -19,14 +21,38 @@ window.addEventListener('load', ()=> {
 			})
 			.then(data => {
 
-				console.log(data);
-				const { temperature, summary } = data.currently;
+				//console.log(data);
+				const {temperature, summary, icon}= data.currently;
 				// Set DOM elements from the api
 				temperatureDegree.textContent = temperature;
-				temperatureDescription = summary; 
+				temperatureDescription.textContent = summary; 
 				locationTimezone.textContent = data.timezone;
 
-		});
-	 });
+				//temperature converter
+				let celcius = (temperature - 32) * (5/9);
+				//set icon
+				setIcons(icon, document.querySelector(".icon"));
+
+
+				//change temp to celcius
+				temperatureSection.addEventListener('click', () =>{
+					if(temperatureSpan.textContent === "F") {
+						temperatureSpan.textContent = "C";
+						temperatureDegree.textContent = Math.floor(celcius);
+					}else{
+						temperatureSpan.textContent = "F";
+						temperatureDegree.textContent = temperature;
+
+					}
+				});
+
+			});
+	 	});
+	}
+	function setIcons(icon, iconID){
+	const skycons = new Skycons({ color: "white" });
+	const currenIcon = icon.replace(/-/g, "_").toUpperCase();
+	skycons.play();
+	return skycons.set(iconID, Skycons[currenIcon]);
 	}
 });
